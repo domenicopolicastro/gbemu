7cf5d99e6134334d4da1cecfd6909b7457332122
domenicopolicastro <domepoli14@gmail.com>
2026-08-19 15:44:55 +0200

    feat: add SDL window and rendering loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,59 @@
 #include <cstdio>
 
 int main(int argc, char* argv[]){
-    SDL_version v;
-    SDL_GetVersion(&v);
-    std::printf("SDL %d.%d.%d\n", v.major, v.minor, v.patch);
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::printf("SDL_Init error: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    SDL_Window* window = SDL_CreateWindow(
+        "Game Boy Emulator",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        160 * 4,
+        144 * 4,
+        0
+    );
+
+    if (window == nullptr) {
+        std::printf("SDL_CreateWindow error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+
+    if (renderer == nullptr) {
+        std::printf("SDL_CreateRenderer error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    int color = 100;
+    while (running) {
+        SDL_SetRenderDrawColor(renderer, color, 150, 100, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        color ++;
+        SDL_Delay(10);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
